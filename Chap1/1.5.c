39860a27b7df7311d4f5edc426311e54c7d42c26
SwampDolphin97 <Kushwah.kartikey14@stu.upes.ac.in>
2018-06-26 15:36:45 +0530

    Book added

--- Chap1/1.5.c
+++ Chap1/1.5.c
@@ -6,6 +6,6 @@ float area, radius = 4;
 int main()
 {
     area = radius*radius*PI;
-    printf("The area of circle is %f cm2 with radius %f cm.", area,radius);
+    printf("The area of circle is %.2f cm2 with radius %.2f cm.", area,radius);
         
 }
